0f32d3980114e705698870e3acf0825d15cb88ab
Pedro Pereira Guimarães <pedro.pereira@gec.inatel.br>
2022-09-26 10:58:58 -0300

    first commit

---
+++ Integracao/src/main/java/br/inatel/Model/User.java
@@ -0,0 +1,22 @@
+package br.inatel.Model;
+
+public class User {
+
+    private String nome;
+    private String cpf;
+
+    private int id;
+
+    public User(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
